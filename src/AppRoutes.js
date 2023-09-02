ee2d686f60df68e251286240a6537254e549c574
thatmike1 <misa.psencik@gmail.com>
2023-09-02 15:28:29 +0200

    additional styling

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Config from "./components/configComponent";
 import FormComponent from "./components/form";
-import ExportToExcel from "./components/exportToExcell";
+// import ExportToExcel from "./components/exportToExcell";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Config />} />
       <Route path="/fourm" element={<FormComponent />} />
-      <Route path="/excell" element={<ExportToExcel />} />
+      {/* <Route path="/excell" element={<ExportToExcel />} /> */}
     </Routes>
   );
 };
